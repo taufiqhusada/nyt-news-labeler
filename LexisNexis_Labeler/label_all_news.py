db68132cb9272a1ba3f1e2d1111ed890291d9508
Taufiq Husada Daryanto <45747001+taufiqhusada@users.noreply.github.com>
2022-02-17 09:13:55 +0700

    Update label_all_news.py

--- LexisNexis_Labeler/label_all_news.py
+++ LexisNexis_Labeler/label_all_news.py
@@ -48,14 +48,14 @@ for filename in tqdm(list_files):
         docs = df['lede'].values
 
         tuple_id_and_outputs = []
-        for i in tqdm(range(len(doc_ids))):
+        for i in range(len(doc_ids)):
             if (docs[i]==None):
                 continue
 
             result = MODEL_ALL.predict(str(docs[i]))
             outputs_this_id = []
             for x in result:
-                outputs_this_id.append(x.label)
+                outputs_this_id.append(f'"{x.label}"')
                 outputs_this_id.append(str(x.score))
 
             tuple_id_and_outputs.append((str(doc_ids[i]), outputs_this_id))
