b37a1f014726031cbcd68bfd9d707c7dd823ae2e
Taufiq Husada Daryanto <45747001+taufiqhusada@users.noreply.github.com>
2022-02-17 09:03:10 +0700

    Update label_all_news.py

--- LexisNexis_Labeler/label_all_news.py
+++ LexisNexis_Labeler/label_all_news.py
@@ -5,6 +5,12 @@ from tqdm import tqdm
 import sys
 import os
 import pandas as pd
+import gc
+
+def read_file(filename):
+    df = pd.read_csv(filename, engine='python')
+    df.fillna('nothing',inplace=True)
+    return df
 
 sys.path.append('..')
 from models import (
@@ -26,46 +32,43 @@ for x in result:
     print(x.label)
     print(x.score)
 
-data_path = '/projectnb/multilm/thdaryan/racial_bias/complete_raw_data_unique'
-list_files = [f'LexisNexis_BostonMedia_NewsArticles_unique_{i}.csv' for i in range(20)]
-    
-for filename in list_files:
+list_files = glob.glob('/projectnb/multilm/yusuf/racial_bias/LexisNexis/output/data/*/*.csv')
+list_files = np.sort(list_files)
+
+with open('result_full/extracted_topic_final.csv', 'w') as outfile:
+    outfile.write('doc_id,label_1,score_1,label_2,score_2,label_3,score_3,label_4,score_4,label_5,score_5\n')
+
+for filename in tqdm(list_files):
+    print(filename)
     try:
-        outfile = open(f'result/labels_{filename[:-4]}.csv', 'w')
-        outfile.write('doc_id,label_1,score_1,label_2,score_2,label_3,score_3,label_4,score_4,label_5,score_5\n')
-
-        print(filename)
-        csv_path = os.path.join(data_path, filename)
-        try:
-            df = pd.read_csv(csv_path, error_bad_lines=False,  encoding='utf8', engine='python')
-            print(df.head(1))
-            doc_ids = df['DOC-ID'].values
-        except:
-            df = pd.read_csv(csv_path, error_bad_lines=False,  encoding='utf8', engine='python', names=['hl1','author','lede','body','DOC-ID','pubDay','pubMonth','pubYear','pubName','filename','Unique_Id'])
-            print(df.head(1))
-            doc_ids = df['DOC-ID'].values
-
+        df = read_file(filename)
+        doc_ids = df['DOC-ID'].values
         docs = df['lede'].values
 
         tuple_id_and_outputs = []
         for i in tqdm(range(len(doc_ids))):
             if (docs[i]==None):
                 continue
+
             result = MODEL_ALL.predict(str(docs[i]))
             outputs_this_id = []
             for x in result:
                 outputs_this_id.append(x.label)
                 outputs_this_id.append(str(x.score))
 
-            outfile.write(str(doc_ids[i]))
-            outfile.write(',')
-            outfile.write(','.join(outputs_this_id))
-            outfile.write('\n')
-            
+            tuple_id_and_outputs.append((str(doc_ids[i]), outputs_this_id))
+        
+        with open('result_full/extracted_topic_final.csv', 'a') as outfile:
+            for (id, outputs_this_id) in tuple_id_and_outputs:
+                outfile.write(id)
+                outfile.write(',')
+                outfile.write(','.join(outputs_this_id))
+                outfile.write('\n')
 
-        outfile.close()
+        del df, doc_ids, docs
+        gc.collect()
 
     except Exception as e:
-        with open('result/errors.txt', 'w') as f:
+        with open('result_full/errors.txt', 'a') as f:
             print(filename, str(e))
-            f.write(filename + " " + str(e))
+            f.write(filename + " " + str(e) + '\n')
