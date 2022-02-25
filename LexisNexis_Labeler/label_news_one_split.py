2fb2ed78076c5737ff5026c27ab963e1bbca1478
Taufiq Husada Daryanto <45747001+taufiqhusada@users.noreply.github.com>
2022-02-25 09:14:43 +0700

    Update label_news_one_split.py

--- LexisNexis_Labeler/label_news_one_split.py
+++ LexisNexis_Labeler/label_news_one_split.py
@@ -37,11 +37,11 @@ for x in result:
 file_split = sys.argv[1] 
 filename = f'/projectnb/multilm/thdaryan/racial_bias/names_extraction_splitted/names_extraction_{file_split}.csv'
 
-with open(f'result_alligned/extracted_topic_{file_split}.tsv', 'w+') as outfile:
+with open(f'result_alligned/extracted_topic_{file_split}.tsv', 'w+', encoding='utf8') as outfile:
     outfile.write('id_from_name_extraction\tDOC-ID\tstart\tend\tsource_type\tlabel\tscore\n')
 
 id_from_name_extraction = 0
-for df_chunk in tqdm(pd.read_csv(filename, chunksize=10**4, header=None)):
+for df_chunk in tqdm(pd.read_csv(filename, chunksize=10**4, header=None,  error_bad_lines=False,  encoding='utf8', engine='python')):
     try:
         df_chunk.rename(columns={0: 'id_from_name_extraction', 1:'sent', 2:'names', 3:'start', 4:'end', 5:'source_type', 6:'DOC-ID'}, inplace=True)
         print(df_chunk.head())
@@ -59,7 +59,7 @@ for df_chunk in tqdm(pd.read_csv(filename, chunksize=10**4, header=None)):
         df_to_save = pd.DataFrame({'id_from_name_extraction':df_chunk['id_from_name_extraction'].values, 'DOC-ID':df_chunk['DOC-ID'].values, 'start': df_chunk['start'].values, 
                                   'end': df_chunk['end'].values, 'label': labels, 'score': scores})
         print(df_to_save.head())
-        df_to_save.to_csv(f'result_alligned/extracted_topic_{folder_name}.tsv', mode='a', header= False, sep='\t')
+        df_to_save.to_csv(f'result_alligned/extracted_topic_{folder_name}.tsv', mode='a', header= False, sep='\t', encoding='utf8')
         
         del df_chunk, df_to_save
         gc.collect()
