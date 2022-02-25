80a1def42ce48a016688dd4d97d04694f24c9282
Taufiq Husada Daryanto <45747001+taufiqhusada@users.noreply.github.com>
2022-02-25 09:53:06 +0700

    Update label_news_one_split.py

--- LexisNexis_Labeler/label_news_one_split.py
+++ LexisNexis_Labeler/label_news_one_split.py
@@ -41,7 +41,7 @@ with open(f'result_alligned/extracted_topic_{file_split}.tsv', 'w+', encoding='u
     outfile.write('id_from_name_extraction\tDOC-ID\tstart\tend\tsource_type\tlabel\tscore\n')
 
 id_from_name_extraction = 0
-for df_chunk in tqdm(pd.read_csv(filename, chunksize=10**4, header=None,  error_bad_lines=False,  encoding='utf-8-sig', engine='python')):
+for df_chunk in tqdm(pd.read_csv(filename, chunksize=10**4, header=None,  error_bad_lines=False,  encoding='utf8', engine='python')):
     try:
         df_chunk.rename(columns={0: 'id_from_name_extraction', 1:'sent', 2:'names', 3:'start', 4:'end', 5:'source_type', 6:'DOC-ID'}, inplace=True)
         print(df_chunk.head())
@@ -51,16 +51,20 @@ for df_chunk in tqdm(pd.read_csv(filename, chunksize=10**4, header=None,  error_
         ids = []
 
         for sent in tqdm(df_chunk['sent'].values):
-            result = MODEL_ALL.predict(str(sent))[0]
-            
-            labels.append(result.label)
-            scores.append(result.score)
+            try:
+                result = MODEL_ALL.predict(sent)[0]
+
+                labels.append(result.label)
+                scores.append(result.score)
+            except:
+                labels.append(None)
+                scores.append(None)
 
         df_to_save = pd.DataFrame({'id_from_name_extraction':df_chunk['id_from_name_extraction'].values, 'DOC-ID':df_chunk['DOC-ID'].values, 'start': df_chunk['start'].values, 
                                   'end': df_chunk['end'].values, 'label': labels, 'score': scores})
      
         print(df_to_save.head())
-        df_to_save.to_csv(f'result_alligned/extracted_topic_{file_split}.tsv', mode='a', header= False, sep='\t', encoding='utf-8-sig')
+        df_to_save.to_csv(f'result_alligned/extracted_topic_{file_split}.tsv', mode='a', header= False, sep='\t', encoding='utf8',index=False)
         
         del df_chunk, df_to_save
         gc.collect()
