fdb22fbf941608f1f833e39927cbce2ee2e7930f
Taufiq Husada Daryanto <45747001+taufiqhusada@users.noreply.github.com>
2022-02-25 13:14:12 +0700

    Create qsub_pararel_per_sentence.sh

---
+++ LexisNexis_Labeler/qsub_pararel_per_sentence.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -l
+
+#$ -P multilm
+#$ -l h_rt=200:00:00   # Specify the hard time limit for the job
+#$ -pe omp 14
+#$ -l mem_per_core=16G
+#$ -N nyt_pararel_alligned
+#$ -j y               # Merge the error and output streams into a single file
+#$ -V
+#$ -m e
+
+python label_news_one_split.py 0 &
+python label_news_one_split.py 1 &
+python label_news_one_split.py 2 &
+python label_news_one_split.py 3 &
+python label_news_one_split.py 4 &
+python label_news_one_split.py 5 &
+python label_news_one_split.py 6 &
+python label_news_one_split.py 7 &
+python label_news_one_split.py 8 &
+python label_news_one_split.py 9 &
+python label_news_one_split.py 10 &
+python label_news_one_split.py 11 &
+wait
+python label_news_one_split.py 12 &
+python label_news_one_split.py 13 &
+python label_news_one_split.py 14 &
+python label_news_one_split.py 15 &
+python label_news_one_split.py 16 &
+python label_news_one_split.py 17 &
+python label_news_one_split.py 18 &
+python label_news_one_split.py 19 &
+python label_news_one_split.py 20 &
+python label_news_one_split.py 21 &
+python label_news_one_split.py 22 &
+python label_news_one_split.py 23 &
+wait
